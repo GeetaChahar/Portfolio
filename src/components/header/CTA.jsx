d0595b2f22ff6e8bff478534c1c2e11ecf0409a6
GEETA CHAHAR <vgeetachahar@gmail.com>
2024-05-08 17:15:17 +0530

    update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
-import { IoMdDownload } from 'react-icons/io'
-import { GiTalk } from 'react-icons/gi'
+import React from "react";
+// import CV from '../../assets/cv.pdf'
+import { IoMdDownload } from "react-icons/io";
+import { GiTalk } from "react-icons/gi";
 
 const CTA = () => {
-    return (
-        <div className='cta'>
-            <a href={CV} download className='btn'><IoMdDownload />  Download CV</a>
-            <a href='#contact' className='btn btn-primary'><GiTalk />  Let's talk</a>
-        </div>
-    )
-}
+  return (
+    <div className="cta">
+      {/* <a href={CV} download className="btn">
+        <IoMdDownload /> Download CV
+      </a> */}
+      <a href="#contact" className="btn btn-primary">
+        <GiTalk /> Let's talk
+      </a>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/me.png'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import "./header.css";
+// import CTA from './CTA'
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-    return (
-        <header>
-            <div className='container header_container' >
-                <h5>Hey this is</h5>
-                <h1>Geeta Chahar</h1>
-                <h5 className='text-light'>Frontend Developer</h5>
-                <CTA />
-                <HeaderSocials />
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hey this is</h5>
+        <h1>Geeta Chahar</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        {/* <CTA /> */}
+        <HeaderSocials />
 
-                <div className='me'>
-                    <img src={ME} alt='' />
-                </div>
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
 
-                <a href='#contact' className='scroll_down'>Scroll Down</a>
-            </div>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
 
-        </header>
-    )
-}
-
-export default Header
+export default Header;
